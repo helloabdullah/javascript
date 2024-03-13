e2a83a801951599782c2dc591b36b05c370ff95a
helloabdullah <abdullahanjum1999@gmail.com>
2024-03-14 01:34:42 +0500

    js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("btn1").onclick = function(){
+    document.getElementById("heading").style.color = "yellow"
+}
+document.getElementById("btn2").onclick = function(){
+    document.getElementById("img2").src = "https://www.w3schools.com/js/pic_bulbon.gif"
+}
+document.getElementById("btn3").onclick = function(){
+    document.getElementById("img2").src = "https://www.w3schools.com/js/pic_bulboff.gif"}
